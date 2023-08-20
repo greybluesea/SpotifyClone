1f8e71cab9a6656f1d042942df2b853f3eac521e
Tony <greybluesea@gmail.com>
2023-08-20 21:47:10 +1200

    btn-big done

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,6 +8,7 @@ import { FaUserAlt } from "react-icons/fa";
 import { toast } from "react-hot-toast"; */
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 /* import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
@@ -82,47 +83,45 @@ const Header = (props: Props) => {
           <BiSearch className="text-BGCOLOR" size={20} />
         </button>
       </div>
-      {/* <div className="flex justify-between items-center gap-x-4">
-          {user ? (
+      <>
+        {
+          /* user */ false ? (
             <div className="flex gap-x-4 items-center">
-              <Button 
-                onClick={handleLogout} 
-                className="bg-white px-6 py-2"
+              <button
+                //   onClick={handleLogout}
+                className="btn-big bg-PRIMARY px-6 py-2"
               >
                 Logout
-              </Button>
-              <Button 
-                onClick={() => router.push('/account')} 
-                className="bg-white"
+              </button>
+              <button
+                onClick={() => router.push("/account")}
+                className="btn-big bg-PRIMARY "
               >
                 <FaUserAlt />
-              </Button>
+              </button>
             </div>
           ) : (
-            <>
-              <div>
-                <Button 
-                  onClick={authModal.onOpen} 
-                  className="
+            <div className="flex gap-x-4 items-center">
+              <button
+                //    onClick={authModal.onOpen}
+                className=" btn-big 
                     bg-transparent 
-                    text-neutral-300 
-                    font-medium
+                    text-PRIMARY 
                   "
-                >
-                  Sign up
-                </Button>
-              </div>
-              <div>
-                <Button 
-                  onClick={authModal.onOpen} 
-                  className="bg-white px-6 py-2"
-                >
-                  Log in
-                </Button>
-              </div>
-            </>
-          )}
-        </div> */}
+              >
+                Sign up
+              </button>
+
+              <button
+                //     onClick={authModal.onOpen}
+                className="btn-big bg-PRIMARY px-6 py-2"
+              >
+                Log in
+              </button>
+            </div>
+          )
+        }
+      </>
     </header>
   );
 };
