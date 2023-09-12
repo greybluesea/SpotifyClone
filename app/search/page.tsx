a5e5f98b18671665abbe3d16415bba8c28ff7517
Tony <greybluesea@gmail.com>
2023-09-12 22:17:52 +1200

    searchbar proper height

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,15 +1,19 @@
 import getSongsByKeywordInTitle from "@/actions/getSongsByKeywordInTitle";
 import PageContent from "@/components/PageContent";
 import SearchInput from "@/components/SearchInput";
+
 import React from "react";
 
 type SearchPageProps = { searchParams: { keyword: string } };
 
+export const revalidate = 0;
+
 const SearchPage = async ({ searchParams: { keyword } }: SearchPageProps) => {
   const songs = await getSongsByKeywordInTitle(keyword);
   return (
-    <main className="px-6 space-y-6">
+    <main className="px-6 pt-0.5 pb-4 space-y-6">
       <SearchInput />
+
       <PageContent songs={songs} />
     </main>
   );
