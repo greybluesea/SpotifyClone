3d20937aa77643b2c3ab329eddc085ed244d8e47
Tony <greybluesea@gmail.com>
2023-09-12 23:36:28 +1200

    close player when !user

--- app/components/Player.tsx
+++ app/components/Player.tsx
@@ -7,14 +7,15 @@ import { useUser } from "@supabase/auth-helpers-react";
 
 import PlayerContent from "./PlayerContent";
 import toast from "react-hot-toast";
+import { useEffect } from "react";
 
 const Player = () => {
-  // const user = useUser();
+  const user = useUser();
   const player = usePlayer();
   const songUrl = useSongUrl(player.activeSong!);
 
   player.activeSong && !songUrl && toast.error("failed to fetch song url");
-  if (!songUrl || !player.activeSong) return null;
+  if (!user || !songUrl || !player.activeSong) return null;
 
   return (
     <div className="sticky bottom-0 left-0 right-0 bg-BGCOLOR w-full py-2 px-4 h-[8dvh] overflow-hidden">
