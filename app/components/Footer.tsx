5683ed1d45cd9b0ffab41cd14684085c7b83cf55
Tony <greybluesea@gmail.com>
2023-09-15 13:43:18 +1200

    "schema change via SQL query"

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,6 +9,8 @@ const Footer = (props: Props) => {
       <ul className="hidden  sm:flex sm:flex-col mt-auto  mx-auto  justify-end items-center bg-transparent space-y-1 px-7 py-5 rounded-xl w-full">
         <li>Supabase ( auth + PostgreSQL + storage )</li>
         <li>Stripe + webhooks (receiving events)</li>
+        <li>schema change via SQL query</li>
+        <li>Radix UI + useSound</li>
         <li>learned from Antonio</li>
         <li>powered by greybluesea</li>
         <li></li>
